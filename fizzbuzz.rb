e04ab1df32276a09713a690a44e3c52cb7af1904
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-28 01:26:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  if n % 3 == 0
+    return "fizz"
+  elsif n % 5 == 0
+    return "buzz"
+  if n % 3 == 0 && n % 5 == 0
+    return "fizzbuzz"
+  end
+end
